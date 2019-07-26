e4c0a1bc2e89c98ca49b32fa65ec95101cc4ee90
wicky <wicky@prideandjoy.org>
2019-07-26 16:42:08 +1200

    sign in page done

--- src/pages/Sign.vue
+++ src/pages/Sign.vue
@@ -2,10 +2,12 @@
   <q-page class="flex flex-center">
     <q-img :src="'statics/img/signin-bg.jpg'" class="fullscreen">
       <div class="absolute-full text-subtitle2 flex flex-center">
-        <div class="column text-center">
-          <h3>Log In</h3>
-          <p>Please log in to your account bellow</p>
-          <q-input filled v-model="text" label="Email" />
+        <div class="column text-center bg-white q-pa-lg shadow-box">
+          <q-img :src="'statics/img/logo.png'" />
+          <p class="text-primary">Please log in to your account bellow</p>
+          <q-input filled v-model="text" label="Email" class="q-pb-sm"/>
+          <q-input filled v-model="password" type="password" label="Password" />
+          <q-btn color="blue" class="full-width q-mt-md" label="Submit" />
         </div>
       </div>
     </q-img>
