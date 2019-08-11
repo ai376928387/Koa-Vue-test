484fe5bbaab9a75394e38462f6c9b265238e489d
wicky <beini.gao@gmail.com>
2019-08-11 12:16:20 +1200

    code moving

--- Server/database.js
+++ Server/database.js
@@ -4,16 +4,17 @@ const initDB = () => {
   mongoose.set('debug', true)
   
   mongoose.connect(
-    'mongodb+srv://wickydb:Qwer1234@cluster0-nasy2.mongodb.net/koa-graphql',
+    'mongodb+srv://wickydb:Qwer1234@cluster0-nasy2.mongodb.net/test',
+    // 'mongodb+srv://wickydb:Qwer1234@cluster0-nasy2.mongodb.net/koa-graphql',
     { useNewUrlParser: true }
   );
 
-  mongoose.connection.on('disconnected', () => {
-    mongoose.connect(config.dbPath)
-  })
-  mongoose.connection.on('error', err => {
-    console.error(err)
-  })
+  // mongoose.connection.on('disconnected', () => {
+  //   mongoose.connect(config.dbPath)
+  // })
+  // mongoose.connection.on('error', err => {
+  //   console.error(err)
+  // })
 
   mongoose.connection.once('open', () => {
     console.log('connected to database');
